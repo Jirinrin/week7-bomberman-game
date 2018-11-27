4911cff861106b8d6a2bb1edb714d99f0fc2f6a9
Jiri Swen <jiri.ei@hotmail.com>
2018-11-27 11:13:10 +0100

    initial commit

--- server/src/games/entities.ts
+++ server/src/games/entities.ts
@@ -1,14 +1,14 @@
-import { BaseEntity, PrimaryGeneratedColumn, Column, Entity, Index, OneToMany, ManyToOne } from 'typeorm'
-import User from '../users/entity'
+import { BaseEntity, PrimaryGeneratedColumn, Column, Entity, Index, OneToMany, ManyToOne } from 'typeorm';
+import User from '../users/entity';
 
-export type Symbol = 'x' | 'o'
-export type Row = [ Symbol | null, Symbol | null, Symbol | null ]
-export type Board = [ Row, Row, Row ]
+export type Symbol = 'x' | 'o';
+export type Row = [ Symbol | null, Symbol | null, Symbol | null ];
+export type Board = [ Row, Row, Row ];
 
-type Status = 'pending' | 'started' | 'finished'
+type Status = 'pending' | 'started' | 'finished';
 
-const emptyRow: Row = [null, null, null]
-const emptyBoard: Board = [ emptyRow, emptyRow, emptyRow ]
+const emptyRow: Row = [null, null, null];
+const emptyBoard: Board = [ emptyRow, emptyRow, emptyRow ];
 
 @Entity()
 export class Game extends BaseEntity {
